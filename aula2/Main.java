6c35f9571d1fbb9a45ed5747bd980e842ec56e3c
joaorooliveira <joaovictorooliveira812@gmail.com>
2024-03-16 11:27:37 -0300

    Adiciona aula2

---
+++ aula2/Main.java
@@ -0,0 +1,12 @@
+package aula2;
+
+public class Main {
+    public static void main(String[] args){
+        Calculadora calc = new Calculadora();
+        calc.numA = 2.0;
+        calc.numB = 3.0;
+
+        System.out.println("Subtrair: " + calc.dividir());
+
+    }
+}
